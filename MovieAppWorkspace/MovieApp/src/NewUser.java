f4ec3f97e3a5c6561a48e550ec81129bd9f25a1b
Mike Simone <mike.simone@live.com>
2017-10-26 10:04:01 -0400

    10/26/17
    Added comments for later features, also formatted file writing to easier distinguish new user fields allowing us to know what line is what info about the user. Can also be used later on to create a profile and account window

--- MovieAppWorkspace/MovieApp/src/NewUser.java
+++ MovieAppWorkspace/MovieApp/src/NewUser.java
@@ -66,11 +66,11 @@ public class NewUser {
 		lblLastName.setBounds(6, 34, 80, 16);
 		frame.getContentPane().add(lblLastName);
 		
-		JLabel lblDateOfBirth = new JLabel("Age:");
-		lblDateOfBirth.setBounds(6, 62, 35, 16);
-		frame.getContentPane().add(lblDateOfBirth);
+		JLabel lblAge = new JLabel("Age:");
+		lblAge.setBounds(6, 62, 35, 16);
+		frame.getContentPane().add(lblAge);
 		
-		JLabel lblUsername = new JLabel("Username:");
+		JLabel lblUsername = new JLabel("Email:");
 		lblUsername.setBounds(6, 90, 80, 16);
 		frame.getContentPane().add(lblUsername);
 		
@@ -86,6 +86,23 @@ public class NewUser {
 		lblSecurityQuestion.setBounds(6, 196, 127, 16);
 		frame.getContentPane().add(lblSecurityQuestion);
 		
+		JLabel lblBlank = new JLabel("~~~");
+		lblBlank.setEnabled(false);
+		lblBlank.setBounds(239, 39, 35, 16);
+		frame.getContentPane().add(lblBlank);
+		lblBlank.setVisible(false);
+		
+		
+		// WHEN RESETTING PASSWORD, IGNORE CASE WHEN
+		// CHECKING SECURITY QUESTION ANSWER
+		JLabel lblInWhatCity = new JLabel("In what city were you born?");
+		lblInWhatCity.setBounds(6, 171, 178, 16);
+		frame.getContentPane().add(lblInWhatCity);
+		
+		
+		// CHECK THAT ALL FIELDS ARE COMPLETED BEFORE ALLOWING REGISTRATION
+		// FAILURE TO FILL ALL FIELDS WILL MAKE A LABEL APPEAR IN RED SAYING
+		// "PLEASE COMPLETE ALL FIELDS BEFORE REGISTERING"
 		firstNameField = new JTextField();
 		firstNameField.setBounds(80, 1, 130, 26);
 		frame.getContentPane().add(firstNameField);
@@ -102,36 +119,47 @@ public class NewUser {
 		ageField.setColumns(10);
 		
 		userField = new JTextField();
-		userField.setBounds(80, 85, 130, 26);
+		userField.setBounds(47, 85, 130, 26);
 		frame.getContentPane().add(userField);
 		userField.setColumns(10);
 		
+		
+		// COMPARE TEXT FROM PASSFIELD AND CONFIRMPASS
+		// IF THEY DO NOT MATCH, MAKE LABEL POP UP
+		// IN RED SAYING "PASSWORDS DO NOT MATCH"
+		// USE .getText() TO PULL THE TYPED INFORMATION
 		passField = new JTextField();
-		passField.setBounds(80, 113, 130, 26);
+		passField.setBounds(70, 113, 130, 26);
 		frame.getContentPane().add(passField);
 		passField.setColumns(10);
 		
 		confirmPass = new JTextField();
-		confirmPass.setBounds(131, 141, 130, 26);
+		confirmPass.setBounds(126, 141, 130, 26);
 		frame.getContentPane().add(confirmPass);
 		confirmPass.setColumns(10);
 		
 		secQuestion = new JTextField();
-		secQuestion.setBounds(116, 191, 130, 26);
+		secQuestion.setBounds(113, 191, 130, 26);
 		frame.getContentPane().add(secQuestion);
 		secQuestion.setColumns(10);
+		
+		blankField = new JTextField();
+		blankField.setBounds(239, 57, 35, 26);
+		frame.getContentPane().add(blankField);
+		blankField.setColumns(10);
+		blankField.setVisible(false);
 		
 		JButton btnReg = new JButton("Register");
 		btnReg.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				try {
-					writeFile(path, firstNameField);
-					writeFile(path, lastNameField);
-					writeFile(path, ageField);
-					writeFile(path, userField);
-					writeFile(path, passField);
-					writeFile(path, secQuestion);
-					writeFile(path, blankField);
+					writeFile(path, firstNameField, lblFirstName);
+					writeFile(path, lastNameField, lblLastName);
+					writeFile(path, ageField, lblAge);
+					writeFile(path, userField, lblUsername);
+					writeFile(path, passField, lblPassword);
+					writeFile(path, secQuestion, lblSecurityQuestion);
+					writeFile(path, blankField, lblBlank);
 					
 				} catch (IOException ioe) {
 					System.out.println("error");
@@ -145,10 +173,6 @@ public class NewUser {
 		btnReg.setBounds(126, 220, 117, 29);
 		frame.getContentPane().add(btnReg);
 		
-		JLabel lblInWhatCity = new JLabel("In what city were you born?");
-		lblInWhatCity.setBounds(6, 171, 178, 16);
-		frame.getContentPane().add(lblInWhatCity);
-		
 		JButton btnNewButton = new JButton("Cancel");
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -159,19 +183,13 @@ public class NewUser {
 		});
 		btnNewButton.setBounds(16, 220, 117, 29);
 		frame.getContentPane().add(btnNewButton);
-		
-		blankField = new JTextField();
-		blankField.setBounds(239, 57, 35, 26);
-		frame.getContentPane().add(blankField);
-		blankField.setColumns(10);
-		blankField.setVisible(false);
 	}
 	
 	// Write to file
-	public void writeFile(String path, JTextField sentField) throws IOException {
+	public void writeFile(String path, JTextField sentField, JLabel sentlbl) throws IOException {
 		FileWriter write = new FileWriter(path, true);
 		PrintWriter prntLine = new PrintWriter(write);
-		prntLine.println(sentField.getText().trim());
+		prntLine.println(sentlbl.getText() + " " + sentField.getText().trim());
 		prntLine.close();
 	}
 }
